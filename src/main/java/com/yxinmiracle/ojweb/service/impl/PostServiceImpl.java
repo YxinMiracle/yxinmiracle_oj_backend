4e7f59ea8be433ccd17ece232afdbb7f11c88ae1
YxinMiracle <939157765@qq.com>
2024-07-02 09:18:54 +0800

    初始化项目

---
+++ src/main/java/com/yxinmiracle/ojweb/service/impl/PostServiceImpl.java
@@ -0,0 +1,166 @@
+package com.yxinmiracle.ojweb.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yxinmiracle.ojweb.common.ErrorCode;
+import com.yxinmiracle.ojweb.constant.CommonConstant;
+import com.yxinmiracle.ojweb.exception.BusinessException;
+import com.yxinmiracle.ojweb.exception.ThrowUtils;
+import com.yxinmiracle.ojweb.mapper.PostMapper;
+import com.yxinmiracle.ojweb.model.dto.post.PostQueryRequest;
+import com.yxinmiracle.ojweb.model.entity.Post;
+import com.yxinmiracle.ojweb.model.entity.User;
+import com.yxinmiracle.ojweb.model.vo.PostVO;
+import com.yxinmiracle.ojweb.model.vo.UserVO;
+import com.yxinmiracle.ojweb.service.PostService;
+import com.yxinmiracle.ojweb.service.UserService;
+import com.yxinmiracle.ojweb.utils.SqlUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
+import cn.hutool.core.collection.CollUtil;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.sort.SortBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.stereotype.Service;
+
+/**
+ * 帖子服务实现
+ */
+@Service
+@Slf4j
+public class PostServiceImpl extends ServiceImpl<PostMapper, Post> implements PostService {
+
+    @Resource
+    private UserService userService;
+
+
+    @Override
+    public void validPost(Post post, boolean add) {
+        if (post == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        String title = post.getTitle();
+        String content = post.getContent();
+        String tags = post.getTags();
+        // 创建时，参数不能为空
+        if (add) {
+            ThrowUtils.throwIf(StringUtils.isAnyBlank(title, content, tags), ErrorCode.PARAMS_ERROR);
+        }
+        // 有参数则校验
+        if (StringUtils.isNotBlank(title) && title.length() > 80) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "标题过长");
+        }
+        if (StringUtils.isNotBlank(content) && content.length() > 8192) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "内容过长");
+        }
+    }
+
+    /**
+     * 获取查询包装类
+     *
+     * @param postQueryRequest
+     * @return
+     */
+    @Override
+    public QueryWrapper<Post> getQueryWrapper(PostQueryRequest postQueryRequest) {
+        QueryWrapper<Post> queryWrapper = new QueryWrapper<>();
+        if (postQueryRequest == null) {
+            return queryWrapper;
+        }
+        String searchText = postQueryRequest.getSearchText();
+        String sortField = postQueryRequest.getSortField();
+        String sortOrder = postQueryRequest.getSortOrder();
+        Long id = postQueryRequest.getId();
+        String title = postQueryRequest.getTitle();
+        String content = postQueryRequest.getContent();
+        List<String> tagList = postQueryRequest.getTags();
+        Long userId = postQueryRequest.getUserId();
+        Long notId = postQueryRequest.getNotId();
+        // 拼接查询条件
+        if (StringUtils.isNotBlank(searchText)) {
+            queryWrapper.and(qw -> qw.like("title", searchText).or().like("content", searchText));
+        }
+        queryWrapper.like(StringUtils.isNotBlank(title), "title", title);
+        queryWrapper.like(StringUtils.isNotBlank(content), "content", content);
+        if (CollUtil.isNotEmpty(tagList)) {
+            for (String tag : tagList) {
+                queryWrapper.like("tags", "\"" + tag + "\"");
+            }
+        }
+        queryWrapper.ne(ObjectUtils.isNotEmpty(notId), "id", notId);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(id), "id", id);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(userId), "userId", userId);
+        queryWrapper.orderBy(SqlUtils.validSortField(sortField), sortOrder.equals(CommonConstant.SORT_ORDER_ASC),
+                sortField);
+        return queryWrapper;
+    }
+
+
+    @Override
+    public PostVO getPostVO(Post post, HttpServletRequest request) {
+        PostVO postVO = PostVO.objToVo(post);
+        long postId = post.getId();
+        // 1. 关联查询用户信息
+        Long userId = post.getUserId();
+        User user = null;
+        if (userId != null && userId > 0) {
+            user = userService.getById(userId);
+        }
+        UserVO userVO = userService.getUserVO(user);
+        postVO.setUser(userVO);
+        return postVO;
+    }
+
+    @Override
+    public Page<PostVO> getPostVOPage(Page<Post> postPage, HttpServletRequest request) {
+        List<Post> postList = postPage.getRecords();
+        Page<PostVO> postVOPage = new Page<>(postPage.getCurrent(), postPage.getSize(), postPage.getTotal());
+        if (CollUtil.isEmpty(postList)) {
+            return postVOPage;
+        }
+        // 1. 关联查询用户信息
+        Set<Long> userIdSet = postList.stream().map(Post::getUserId).collect(Collectors.toSet());
+        Map<Long, List<User>> userIdUserListMap = userService.listByIds(userIdSet).stream()
+                .collect(Collectors.groupingBy(User::getId));
+
+
+        // 填充信息
+        List<PostVO> postVOList = postList.stream().map(post -> {
+            PostVO postVO = PostVO.objToVo(post);
+            Long userId = post.getUserId();
+            User user = null;
+            if (userIdUserListMap.containsKey(userId)) {
+                user = userIdUserListMap.get(userId).get(0);
+            }
+            postVO.setUser(userService.getUserVO(user));
+            return postVO;
+        }).collect(Collectors.toList());
+        postVOPage.setRecords(postVOList);
+        return postVOPage;
+    }
+
+}
+
+
+
+
